676e326f20f3e0cf743eee158a2145a9a945b819
sanketkumar-beato <43717380+sanketkumar-beato@users.noreply.github.com>
2024-09-10 09:32:15 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -10,39 +10,39 @@ from youtube_video import YouTubeVideo
 
 
 def main():
-    st.title("Document Summarizer")
+    st.title("Diagnostics Report Summarizer")
 
-    input_method = st.radio(
-        "Select input method", ("Upload a document", "Enter a YouTube URL")
+    # input_method = st.radio(
+    #     "Select input method", ("Upload a document", "Enter a YouTube URL")
+    # )
+
+    # if input_method == "Upload a document":
+    uploaded_file = st.file_uploader(
+        "Upload a document",
+        type=["pdf"],
     )
 
-    if input_method == "Upload a document":
-        uploaded_file = st.file_uploader(
-            "Upload a document",
-            type=["pdf"],
-        )
+    # if input_method == "Enter a YouTube URL":
+    #     youtube_url = st.text_input("Enter a YouTube URL")
 
-    if input_method == "Enter a YouTube URL":
-        youtube_url = st.text_input("Enter a YouTube URL")
-
-    st.sidebar.markdown("# [Contact me by email!](mailto:ethanujohnston@gmail.com)")
-    st.sidebar.markdown(
-        "# [Check out my other projects!](https://github.com/e-johnstonn)"
-    )
-    st.sidebar.markdown("# [Twitter / X](https://x.com/ethanjdev)")
+    st.sidebar.markdown("# [Contact](mailto:sanketkumar.biswas@beatoapp.com)")
+    # st.sidebar.markdown(
+    #     "# [Check out my other projects!](https://github.com/e-johnstonn)"
+    # )
+    # st.sidebar.markdown("# [Twitter / X](https://x.com/ethanjdev)")
 
     if st.button("Summarize"):
-        if input_method == "Upload a document":
-            if uploaded_file is None:
-                st.warning("Please upload a file.")
-                return
-            summarize_file(uploaded_file)
+        # if input_method == "Upload a document":
+        if uploaded_file is None:
+            st.warning("Please upload a file.")
+            return
+        summarize_file(uploaded_file)
 
-        else:
-            if not youtube_url:
-                st.warning("Please enter a YouTube URL.")
-                return
-            summarize_youtube(youtube_url)
+        # else:
+        #     if not youtube_url:
+        #         st.warning("Please enter a YouTube URL.")
+        #         return
+        #     summarize_youtube(youtube_url)
 
 
 def summarize_file(uploaded_file: UploadedFile):
@@ -56,16 +56,16 @@ def summarize_file(uploaded_file: UploadedFile):
     st.markdown(run_summary(document.text_content, f"document"), unsafe_allow_html=True)
 
 
-def summarize_youtube(youtube_url: str):
-    video = YouTubeVideo(youtube_url)
-    transcript = video.get_transcript()
-    validation_errors = Validator.validate_text(transcript)
-
-    if validation_errors:
-        st.warning(validation_errors)
-        return
-
-    st.markdown(run_summary(transcript, "youtube video"), unsafe_allow_html=True)
+# def summarize_youtube(youtube_url: str):
+#     video = YouTubeVideo(youtube_url)
+#     transcript = video.get_transcript()
+#     validation_errors = Validator.validate_text(transcript)
+# 
+#     if validation_errors:
+#         st.warning(validation_errors)
+#         return
+# 
+#     st.markdown(run_summary(transcript, "youtube video"), unsafe_allow_html=True)
 
 
 def run_summary(text: str, media_type: str):
